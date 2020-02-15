a51677c4403bc25d19d50f43f781c0f0c1f10736
Burak Başeskioğlu <burakbaseskioglu@hotmail.com>
2020-02-16 00:40:43 +0300

    Admin panel has been edited. Department section added to a new controller

--- DepartmentPersonel.Entities/Personel.cs
+++ DepartmentPersonel.Entities/Personel.cs
@@ -10,13 +10,20 @@ namespace DepartmentPersonel.Entities
     public class Personel
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage ="Personel adı boş geçilemez"), Display(Name="Personel Adı"), 
             StringLength(50, ErrorMessage ="Lütfen 50 karakterden fazla değer girmeyiniz")]
         public string Name { get; set; }
+
         [Required(ErrorMessage ="Personel soyadı boş geçilemez"), Display(Name="Personel Soyadı"), 
             StringLength(70, ErrorMessage ="Lütfen 70 karakterden fazla değer girmeyiniz")]
         public string LastName { get; set; }
+
+        //[Required(ErrorMessage = "Personel maaşı boş geçilemez"), Display(Name="Personel Maaşı"),
+        //    Range(1,100000, ErrorMessage ="Personel maaşı en fazla 100.000 olabilir")]
+        //public decimal Salary { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
+
     }
 }
--- DepartmentPersonel.WebUI/Controllers/HomeController.cs
+++ DepartmentPersonel.WebUI/Controllers/HomeController.cs
@@ -12,76 +12,16 @@ namespace DepartmentPersonel.WebUI.Controllers
     public class HomeController : Controller
     {
         private IDepartmentService _departmentService;
+        private IPersonelService _personelService;
 
-        public HomeController(IDepartmentService departmentService)
+        public HomeController(IDepartmentService departmentService, IPersonelService personelService)
         {
             _departmentService = departmentService;
+            _personelService = personelService;
         }
-        // GET: Home
         public ActionResult Index()
-        {
-            return View(_departmentService.GetAll());
-        }
-
-        public ActionResult Create()
         {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Create(Department department)
-        {
-            if (ModelState.IsValid)
-            {
-                _departmentService.Insert(department);
-                _departmentService.Save();
-                return RedirectToAction("Index");
-            }
-            return View(department);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            Department department = _departmentService.GetById(id);
-            if(department == null)
-            {
-                throw new Exception("Departmant bulunamadı");
-            }
-            
-            return View(department);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(Department department)
-        {
-            if(ModelState.IsValid)
-            {
-                _departmentService.Update(department);
-                _departmentService.Save();
-                return RedirectToAction("Index");
-            }
-            return View(department);
-        }
-
-        public ActionResult Delete(int id)
-        {
-            Department department = _departmentService.GetById(id);
-            if(department == null)
-            {
-                throw new Exception("Silinecek departman bulunamadı");
-            }
-            try
-            {
-                _departmentService.Delete(id);
-                _departmentService.Save();
-                return RedirectToAction("Index");
-            }
-            catch (Exception)
-            {
-                throw new Exception("Departman silinemedi");
-            }
-        }
-
-
     }
 }
